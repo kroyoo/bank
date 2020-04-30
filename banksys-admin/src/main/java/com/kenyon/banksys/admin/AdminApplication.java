697719234d510ef55e740a562a661bd64f2cab16
GodYeksha <GodYeksha@gmail.com>
2020-04-30 04:10:39 -0500

    Springboot admin监控

---
+++ banksys-admin/src/main/java/com/kenyon/banksys/admin/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.kenyon.banksys.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Kenyon
+ * @title: AdminApplication
+ * @projectName bank
+ * @description: TODO
+ * @date 9/6/201912:34 AM
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
