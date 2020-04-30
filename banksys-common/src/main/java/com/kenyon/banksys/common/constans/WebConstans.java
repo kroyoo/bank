f31a41d00c09901979ad2cb75c949f032498cfa6
GodYeksha <GodYeksha@gmail.com>
2020-04-30 04:04:12 -0500

    公共方法|工具|RESTful

---
+++ banksys-common/src/main/java/com/kenyon/banksys/common/constans/WebConstans.java
@@ -0,0 +1,13 @@
+package com.kenyon.banksys.common.constans;
+
+/**
+ * @author Kenyon
+ * @title: WebConstans
+ * @projectName bank
+ * @description: TODO
+ * @date 9/12/20191:30 AM
+ */
+public class WebConstans {
+    public static final String SEESION_UER = "admin";
+    public static final String SEESION_TOKEN = "token";
+}
